dc8c94dd13a7371fbe8278b9b5e1faf6b247fc7b
Gathoni <janetgathoni910@gmail.com>
2023-09-30 22:30:50 +0300

    Installed flask-sqlAlchemy

--- server/app.py
+++ server/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///superheroes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
 
 @app.route('/')
