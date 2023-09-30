fbb30f83d52521daf75913da56620da77413de76
Gathoni <janetgathoni910@gmail.com>
2023-09-30 20:29:14 +0300

    Created Models

---
+++ server/models.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero (db.model):
+    __tablename__ = "heroes"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    
+    
+class Hero_power (db.model):
+    __tablename__ = "hero_powers"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id= db.Column(db.Interger)
+    power_id= db.Column(db.Interger)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    
+class Power (db.model):
+    __tablename__ = "powers"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    
