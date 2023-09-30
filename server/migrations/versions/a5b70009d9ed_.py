7292155a30f1ba2a492816333f01c487dde1c261
Gathoni <janetgathoni910@gmail.com>
2023-09-30 22:32:35 +0300

    Configured migrations

---
+++ server/migrations/versions/a5b70009d9ed_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a5b70009d9ed
+Revises: 
+Create Date: 2023-09-30 22:24:03.012618
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a5b70009d9ed'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
