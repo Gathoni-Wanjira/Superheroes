23f2125d98e805918bf9e67ef9fa4f1213f05940
Gathoni <janetgathoni910@gmail.com>
2023-09-30 22:31:27 +0300

    Powers Relationship

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Hero (db.model):
+class Hero (db.Model):
     __tablename__ = "heroes"
     
     id = db.Column(db.Integer, primary_key=True)
@@ -14,20 +14,20 @@ class Hero (db.model):
     hero_powers = db.relationship('Hero_power', backref='hero', lazy=True)
     
     
-class Hero_power (db.model):
+class Hero_power (db.Model):
     __tablename__ = "hero_powers"
     
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
-    hero_id= db.Column(db.Interger,db.ForeignKey('heroes.id'), nullable=False)
-    power_id= db.Column(db.Interger,db.ForeignKey('powers.id'), nullable=False)
+    hero_id= db.Column(db.Integer,db.ForeignKey('heroes.id'), nullable=False)
+    power_id= db.Column(db.Integer,db.ForeignKey('powers.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
     
     hero = db.relationship('Hero', backref='hero_powers')
     power = db.relationship('Power', backref='hero_powers')
     
-class Power (db.model):
+class Power (db.Model):
     __tablename__ = "powers"
     
     id = db.Column(db.Integer, primary_key=True)
@@ -38,3 +38,5 @@ class Power (db.model):
     
     
     heroes = db.relationship('Hero_power', backref='power', lazy=True)
+    
+    
