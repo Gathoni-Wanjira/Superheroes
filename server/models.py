ba27dda669c3ab051aa866d45e80aab384a28948
Gathoni <janetgathoni910@gmail.com>
2023-09-30 21:32:19 +0300

    Created Model relationships

--- server/models.py
+++ server/models.py
@@ -11,16 +11,21 @@ class Hero (db.model):
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
     
+    hero_powers = db.relationship('Hero_power', backref='hero', lazy=True)
+    
     
 class Hero_power (db.model):
     __tablename__ = "hero_powers"
     
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
-    hero_id= db.Column(db.Interger)
-    power_id= db.Column(db.Interger)
+    hero_id= db.Column(db.Interger,db.ForeignKey('heroes.id'), nullable=False)
+    power_id= db.Column(db.Interger,db.ForeignKey('powers.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
+    
+    hero = db.relationship('Hero', backref='hero_powers')
+    power = db.relationship('Power', backref='hero_powers')
     
 class Power (db.model):
     __tablename__ = "powers"
